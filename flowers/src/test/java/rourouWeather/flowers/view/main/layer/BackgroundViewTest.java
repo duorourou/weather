2c48b27f318c68c96b0427c3c5732f9fa2b67ad9
hardycheng <xchou@localhost.localdomain>
2015-12-11 17:02:15 +0800

    add the test class for backgroundView

---
+++ flowers/src/test/java/rourouWeather/flowers/view/main/layer/BackgroundViewTest.java
@@ -0,0 +1,16 @@
+package rourouWeather.flowers.view.main.layer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xchou on 12/11/15.
+ */
+public class BackgroundViewTest {
+
+    @Test
+    public void testPaintComponent() throws Exception {
+
+    }
+}
